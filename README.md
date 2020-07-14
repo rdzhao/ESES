c1062d5fa09aa9705eb82330650d520dc0fdc31b
Rundong Zhao <zhaorund@msu.edu>
2020-07-13 21:49:11 -0400

    clear

--- README.md
+++
@@ -1,2 +0,0 @@
-# ESES
-Software for Eulerian solvent excluded surface (SES). It geneates the SES in a Eulerian regular grid with parallel treatment.
--- convex_edge.cpp
+++
@@ -1,7 +0,0 @@
-#include "convex_edge.h"
-
-convex_edge::convex_edge(CVector3d p1, CVector3d p2, int index){
-	pos1 = p1;
-	pos2 = p2;
-	atom_index = index;
-}
